47e71d3244b1b696c34ec8c3c6b959988e2f2b82
Yannic <me@yannic.at>
2024-02-16 18:20:01 +0100

    stuff

---
+++ components/content/Columns.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="col-span-x">
+		<ContentSlot />
+	</div>
+</template>
+
+<script setup lang="ts">
+const { width } = defineProps({
+	width: {
+		type: Number,
+		default: 1,
+	},
+});
+</script>
+
+<style scoped>
+.col-span-x {
+	grid-column: span v-bind(width) / span v-bind(width);
+}
+</style>
---
+++ components/content/Row.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="grid-cols-x">
+		<ContentSlot />
+	</div>
+</template>
+
+<script setup lang="ts">
+const { columns } = defineProps({
+	columns: {
+		type: Number,
+		default: 1,
+	},
+});
+</script>
+
+<style scoped>
+.grid-cols-x {
+	display: grid;
+	grid-template-columns: repeat(calc(v-bind(columns)), minmax(0, 1fr));
+}
+</style>
