f79411d6870db7c31ab67b494361a9bd38eb9614
Yannic <me@yannic.at>
2024-02-16 18:36:04 +0100

    test

---
+++ components/content/Row.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="grid-cols-x">
+		<slot />
+	</div>
+</template>
+
+<script setup lang="ts">
+const { columns } = defineProps({
+	columns: {
+		type: Number,
+		default: 1,
+	},
+});
+</script>
+
+<style scoped>
+.grid-cols-x {
+	display: grid;
+	grid-template-columns: repeat(v-bind(columns), minmax(0, 1fr));
+}
+</style>
--- components/content/RowDefault.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	123
-	<div>
-		<slot />
-	</div>
-</template>
